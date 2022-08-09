5e960b1ed32b44398c5dbf0bcd7d6218cd5aa517
Kristine Kochie <kristinekochie@gmail.com>
2022-08-09 09:52:16 -0400

    commit tree dd71273c3be196037bbf9e84683e2481d706ff22

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
+  # Use query params in the index action to return a list of students whose first or last name matches the query parameter
   def index
-    students = Student.all
+    students =  if params[:name]
+                  Student.by_name(params[:name])
+                else
+                  Student.all
+                end
     render json: students
   end
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,11 @@
 class Student < ApplicationRecord
-
+  #Combine first_name and last_name with a space
   def to_s
     "#{first_name} #{last_name}"
   end
 
+  #find by name
+  def self.by_name(name)
+    Student.where("first_name LIKE ? OR last_name LIKE ?", "%#{name}", "%#{name}")
+  end
 end
